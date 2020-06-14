70ae31c2daffd10b4e84e4641379ce947bf53e47
Võ Tuấn Nguyên <tuannguyen8888@gmail.com>
2020-06-14 22:50:30 +0700

    fix bug delete supplier

--- Database script/fix bug delete supplier.sql
+++ Database script/fix bug delete supplier.sql
@@ -1,1 +1,3 @@
 UPDATE cms_foreign_key_mapping SET `child_field` = 'object_id', `cond` = 'object_type = 1' WHERE (`id` = '13');
+UPDATE cms_foreign_key_mapping SET `cond` = 'object_type = 2' WHERE (`id` = '25');
+
